6510aa49863b11b01b9b624c54451e3f03e462bd
Bhavesh Patil <bhavesh.patil@perennialsys.com>
2023-01-09 15:27:41 +0530

    Initial changes

--- app/src/main/java/com/perennial/android_actions/MainActivity.kt
+++ app/src/main/java/com/perennial/android_actions/MainActivity.kt
@@ -2,10 +2,12 @@ package com.perennial.android_actions
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(this, "No activities found", Toast.LENGTH_SHORT).show()
     }
 }
